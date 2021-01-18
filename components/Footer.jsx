d087f217518bc7875671033e8806430e1574e0ca
Jesus Wisord <jesuswduarte@gmail.com>
2021-01-17 18:35:15 -0600

    [ADD] Footer info

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import styles from '../styles/Footer.module.scss';
 
 const Footer = () => (
-  <footer className={styles.footer} />
+  <footer className={styles.footer}>
+    <p>
+      Avenida general ignacio zaragoza #1731 col.
+      La nueva mexicali b.c. de lunes a viernes de 8:00 am 4:00 pm
+    </p>
+  </footer>
 );
 
 export default Footer;
