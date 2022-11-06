3ead6615c02c123996789b440ebe5167c5279902
Chuy <jduarte@nexu.mx>
2022-11-06 15:57:04 -0600

    fix: fixed new hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,18 +3,15 @@ import styles from '../styles/Hero.module.scss';
 
 const Hero = () => (
   <div className={styles.hero}>
-    <img src="/car.png" alt="" />
-    <div className={styles.right_side}>
-      <p>
-        Somos una empresa en soluciones financieras, líderes en el
+    <figure>
+      <img src="/hero.jpg" alt="" />
+    </figure>
+    <div className={styles.float}>
+      <h2>
+        Empresa en soluciones financieras, líderes en el
         mercado sobre el préstamo de automotores, estamos
         listos para apoyarte.
-      </p>
-      <p>
-        Si necesitas efectivo y tu automotor puede ser la solución,
-        pero no conoces los requisitos, modalidades y beneficios que te ofrecemos,
-        acércate a nosotros para que podamos ayudarte y apoyarte durante el proceso de empeño.
-      </p>
+      </h2>
       <button type="button">
         <a href="#contact">
           ¿Quieres empeñar?
