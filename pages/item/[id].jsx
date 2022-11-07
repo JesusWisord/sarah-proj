edee75239ade2f851a06849c87be471b1fdb80ff
Chuy <jduarte@nexu.mx>
2022-11-06 22:22:54 -0600

    quick fix for images null

--- pages/item/[id].jsx
+++ pages/item/[id].jsx
@@ -17,21 +17,21 @@ export default function Item() {
       </Head>
       <Header />
       <figure className={styles.hero}>
-        <img src={`/new_catalogue/${data[id].images[0]}`}></img>
+        <img src={`/new_catalogue/${data[id]?.images[0]}`} />
       </figure>
       <p className={styles.info}>
         {data[id].info}
       </p>
       <div className={styles.catalogue_container}>
-        {data[id].images.map((item, index) => (
-            <CatalogueItem
-              image={item}
-              id={index}
-              disableLink={true}
-            />
-          ))}
+        {data[id]?.images.map((item, index) => (
+          <CatalogueItem
+            image={item}
+            id={index}
+            disableLink
+          />
+        ))}
       </div>
       <Footer />
     </>
   );
-};
+}
